55bcd94565330be130938cb15c07d60e107d2717
tmnt66 <branzil.nbj@gmail.com>
2022-05-30 13:44:57 +0530

    added new files

---
+++ src/hompage.component.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './homepage.styles.scss'
+const HomePage = () => {
+    return (
+
+        <div className='Hompage'>
+            <div className='directory-menu'>
+                <div className='menuitems'>
+                    <div className='content'>
+                        <h1 className='title'>HATS
+                        </h1>
+                        <span className='shopnow'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menuitems'>
+                    <div className='content'>
+                        <h1 className='title'>JACKETS
+                        </h1>
+                        <span className='shopnow'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menuitems'>
+                    <div className='content'>
+                        <h1 className='title'>Sneakers
+                        </h1>
+                        <span className='shopnow'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menuitems'>
+                    <div className='content'>
+                        <h1 className='title'>Pants
+                        </h1>
+                        <span className='shopnow'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menuitems'>
+                    <div className='content'>
+                        <h1 className='title'>Trousers
+                        </h1>
+                        <span className='shopnow'>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default HomePage;
